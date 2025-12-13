1b29d2f2ebfbedda3ae4810e7265d8a08a4ba6c4
Mohammed Saleh <kaebs19@gmail.com>
2025-12-13 04:35:04 +0300

    Initial Commit

---
+++ Seerati/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Seerati
+//
+//  Created by Mohammed Saleh on 13/12/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
